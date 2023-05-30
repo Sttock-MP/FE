f159af2a398c53148cafa8efd96abdf0743eefbf
yeokyoung song <spolice0324@gmail.com>
2023-05-30 17:29:29 +0900

    [feat] 메인 페이지 상품 추가하기 플로팅 버튼 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,3 +1,51 @@
+import { useState, useEffect } from 'react'
+
+import React from 'react'
+import 스똑메인1 from '../components/MainSlide/스똑메인 1.jpg'
+import 스똑메인2 from '../components/MainSlide/스똑메인 2.jpg'
+import 스똑메인3 from '../components/MainSlide/스똑메인 3.jpg'
+import 스똑메인4 from '../components/MainSlide/스똑메인 4.jpg'
+import 스똑메인5 from '../components/MainSlide/스똑메인 5.jpg'
+
 export default function MainPage() {
-  return <div>메인페이지</div>
+  const [top, setTop] = useState(300)
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+  const handleScroll = () => {
+    if (window.scrollY >= 100) {
+      setTop(650)
+    } else {
+      setTop(300)
+    }
+  }
+
+  return (
+    <div>
+      <img src={스똑메인1}></img>
+      <img src={스똑메인4}></img>
+      <img src={스똑메인5}></img>
+      <img src={스똑메인3}></img>
+      <img src={스똑메인2}></img>
+      {/* <div className="fixed top-[650px] left-1/2  transform -translate-x-1/2 -translate-y-1/2">
+        <button className="w-[120px] h-[50px] rounded-full bg-darkBrown text-white text-md">
+          상품추가하기
+        </button>
+      </div>
+     
+     플로팅버튼  */}
+
+      <div
+        className="fixed left-1/2 transform -translate-x-1/2"
+        style={{ top: `${top > 330 ? 580 : 400}px`, transition: 'top 0.5s ease-in-out' }}
+      >
+        <div className="relative">
+          <button className="w-[120px] h-[50px] rounded-full bg-darkBrown text-white text-md">
+            상품추가하기
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
